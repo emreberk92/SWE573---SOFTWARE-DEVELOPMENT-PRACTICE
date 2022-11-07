4ba09ecf415152b41284341013652028952a1594
Mehmet Emre Berk <115401843+emreberk92@users.noreply.github.com>
2022-11-07 19:28:44 +0300

    This to code for basic web application for milestone 1

---
+++ SWE573_Basic_Web_App/src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private UserRepository repo;
+
+    public AppController(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String showSignupForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegistration(User user){
+        repo.save(user);
+        return "register_success";
+    }
+
+    @GetMapping("/list_users")
+    public String viewUserList() {
+        return "users";
+    }
+
+}
---
+++ SWE573_Basic_Web_App/src/main/java/com/example/demo/Swe573BasicWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swe573BasicWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swe573BasicWebAppApplication.class, args);
+	}
+
+}
