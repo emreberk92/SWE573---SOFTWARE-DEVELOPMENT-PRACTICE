3d8a119eb76fc961bbb0429aae4bffb24d4598c5
Mehmet Emre Berk <115401843+emreberk92@users.noreply.github.com>
2022-11-20 18:53:18 +0300

    Add files via upload

---
+++ authentication/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    path('signout', views.signout, name="signout"),
+    path('setprofile', views.setprofile, name="setprofile"),
+    path('profile', views.profile, name="profile"),
+   # path('activate/<uidb64>/<token>', views.activate, name="activate"),
+]
